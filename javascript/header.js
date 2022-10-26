988533c360bd56e89e65b0fb3773b8a02d155c2a
unknown <longho964@gmail.com>
2022-10-26 09:17:17 +0700

    update css header

--- javascript/header.js
+++ javascript/header.js
@@ -1,9 +1,11 @@
 const slideBarElm = document.getElementById("slidebar");
 const closeSlideBarElm = document.getElementById("line");
-const openModalImageElm = document.querySelector(".slidebar-image");
+const openModalImageElm = document.querySelectorAll(".slidebar-image");
+// const openModalImageElm = document.querySelector(".slidebar-image");
+
 const closeImageElm = document.querySelector(".modal-closebutton");
 
-console.log(openModalImageElm);
+console.log("open", openModalImageElm);
 slideBarElm.addEventListener("click", (e) => {
   const showSlider = document.querySelector(".slidebar-container");
   showSlider.style.display = "block";
@@ -14,14 +16,15 @@ closeSlideBarElm.addEventListener("click", (e) => {
   closeSlideBar.style.display = "none";
 });
 
-openModalImageElm.addEventListener("click", (e) => {
-  const openmodalimage = document.getElementById("modal-container");
-  openmodalimage.style.display = "block";
-});
-
 closeImageElm.addEventListener("click", (e) => {
-  let imageNumber = openModalImageElm;
-
   const closeimage = document.getElementById("modal-container");
   closeimage.style.display = "none";
 });
+
+for (let i = 0; i <= openModalImageElm.length; i++) {
+  console.log(openModalImageElm[i].src);
+  openModalImageElm[i].addEventListener("click", (e) => {
+    const openmodalimage = document.getElementById("modal-container");
+    openmodalimage.style.display = "block";
+  });
+}
