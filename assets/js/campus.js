1a6b237f00fdd575d81dfa422f5a4d7e7ed31820
unknown <longho964@gmail.com>
2022-10-27 17:04:58 +0700

    update image

--- assets/js/campus.js
+++ assets/js/campus.js
@@ -1,17 +1,17 @@
-export const initCampus = [ 
+export const initCampus = [
   {
     id: 1,
     title: "One plus (+) Programing foundation",
-    image: "../image/campus/one_plus.png",
+    image: "../image/campus/one_plus.webp",
   },
   {
     id: 2,
     title: "Two plus (++) Skill up to to get ready",
-    image: "../image/campus/two_plus.png",
+    image: "../image/campus/two_plus_1.webp",
   },
   {
     id: 3,
     title: "Three plus (+++) How to become a senior",
-    image: "../image/campus/three_plus.png",
+    image: "../image/campus/three_plus_1.webp",
   },
-]
+];
--- assets/js/courses.js
+++ assets/js/courses.js
@@ -1,56 +1,56 @@
-export const intCourses = [ 
+export const intCourses = [
   {
     id: 1,
     name: "Programing foundation",
     desc: "Algorithm, Design pattern",
-    image: "../image/categories/icons/icon-1.png"
+    image: "../image/categories/icons/icon-1.webp",
   },
   {
     id: 2,
     name: "Database",
     desc: "SQL Syntax",
-    image: "../image/categories/icons/icon-2.png"
+    image: "../image/categories/icons/icon-2.webp",
   },
   {
     id: 3,
     name: "Testing",
     desc: "Unit testing, TDD",
-    image: "../image/categories/icons/icon-3.png"
+    image: "../image/categories/icons/icon-3.webp",
   },
   {
     id: 4,
     name: "Front-end",
     desc: "HTML/CSS/Js , ReactJS/VueJS",
-    image: "../image/categories/icons/icon-4.png"
+    image: "../image/categories/icons/icon-4.webp",
   },
   {
     id: 5,
     name: "Back-end",
     desc: "PHP, NodeJS",
-    image: "../image/categories/icons/icon-5.png"
+    image: "../image/categories/icons/icon-5.webp",
   },
   {
     id: 6,
     name: "Agile Stable Team",
     desc: "Agile/Scrum",
-    image: "../image/categories/icons/icon-6.png"
+    image: "../image/categories/icons/icon-6.webp",
   },
   {
     id: 7,
     name: "English",
     desc: "Basic conversion",
-    image: "../image/categories/icons/icon-7.png"
+    image: "../image/categories/icons/icon-7.webp",
   },
   {
     id: 8,
     name: "Communication",
     desc: "HORENSO",
-    image: "../image/categories/icons/icon-8.png"
+    image: "../image/categories/icons/icon-8.webp",
   },
   {
     id: 9,
     name: "Career path",
     desc: "Work ethic, Integrity",
-    image: "../image/categories/icons/icon-9.png"
+    image: "../image/categories/icons/icon-9.webp",
   },
-]
+];
