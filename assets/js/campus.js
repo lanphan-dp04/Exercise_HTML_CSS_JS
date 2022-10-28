0d446b52bbd46c0d99703567820796a035de418b
lanphan_dp04 <lan.phan_dp04@devplus.edu.vn>
2022-10-28 11:17:36 +0700

    fix bug deploy

--- assets/js/campus.js
+++ assets/js/campus.js
@@ -2,16 +2,16 @@ export const initCampus = [
   {
     id: 1,
     title: "One plus (+) Programing foundation",
-    image: "../image/campus/one_plus.webp",
+    image: "../assets/image/campus/one_plus.webp",
   },
   {
     id: 2,
     title: "Two plus (++) Skill up to to get ready",
-    image: "../image/campus/two_plus_1.webp",
+    image: "../assets/image/campus/two_plus_1.webp",
   },
   {
     id: 3,
     title: "Three plus (+++) How to become a senior",
-    image: "../image/campus/three_plus_1.webp",
+    image: "../assets/image/campus/three_plus_1.webp",
   },
 ];
